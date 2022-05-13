3ed057c85ed66022bbe5bb7743d8396f4048f89d
Prabhu Kumar Chirra <prabhuchira@gmail.com>
2022-05-13 22:21:21 +0530

    Changes

--- shared/Footer/Footer.jsx
+++ shared/Footer/Footer.jsx
@@ -4,7 +4,7 @@
 export default function Footer(){
     return(
       
-    <div  class="text-center bg-dark  py-3 text-muted" style={{paddingBlock:'30px !important',position:"sticky",width:'100%',bottom:'0px'}}>
+    <div  class="text-center bg-dark  py-3 text-muted" style={{paddingBlock:'30px !important'}}>
 
       <div class="text-light lato-heading" >
 
